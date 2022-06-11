1e176e8e74e99ccf9d32d64d46a9f43eabb3c84b
Sergey <bonadysev@gmail.com>
2022-06-11 18:09:07 +0300

    Updates

---
+++ precache-manifest.f0954ba37dbdc137da7e20f13bda9150.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fed20418a2be326d0fb34d5905be73b",
+    "url": "/simple-react-homeworks-master/index.html"
+  },
+  {
+    "revision": "7ac8c66348ad881823f2",
+    "url": "/simple-react-homeworks-master/static/css/main.635898b2.chunk.css"
+  },
+  {
+    "revision": "23c9b46dbab398bd1b73",
+    "url": "/simple-react-homeworks-master/static/js/2.e03fc32e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-homeworks-master/static/js/2.e03fc32e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac8c66348ad881823f2",
+    "url": "/simple-react-homeworks-master/static/js/main.c0f31fbf.chunk.js"
+  },
+  {
+    "revision": "33f020c42a00208bc7a5",
+    "url": "/simple-react-homeworks-master/static/js/runtime-main.074d676d.js"
+  },
+  {
+    "revision": "252b98cef23b50fa08f303966e5cea98",
+    "url": "/simple-react-homeworks-master/static/media/checked.252b98ce.png"
+  }
+]);
