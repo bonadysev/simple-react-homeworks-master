7d3cdb4c252e2702ff0e5966354ebe7fae152585
Sergey <bonadysev@gmail.com>
2022-08-17 18:01:31 +0300

    17-08-2022

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -10,7 +10,8 @@ function HW6() {
         saveState<string>('editable-span-value', value)
     }
     const restore = () => {
-        // setValue()
+        const edSpValue = restoreState<string>('editableSpanValue','')
+        setValue(edSpValue)
     }
 
     return (
